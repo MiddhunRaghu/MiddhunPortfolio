0c09fb927b73b2b08b4b466c643bec2509c116b2
MiddhunRaghu <middhunraghu@gmail.com>
2025-05-08 12:15:19 +0530

    spotify url update

--- src/Sections/Projects/Projects.jsx
+++ src/Sections/Projects/Projects.jsx
@@ -10,7 +10,7 @@ function Projects() {
     <section id="projects" className={styles.container}>
         <h1 className='sectionTitle'>Projects</h1>
         <div className={styles.projectsContainer}>
-            <ProjectCard src={SpotifyIcon} link="" h3="Spotify" p="Audio Streaming WebApp"/>
+            <ProjectCard src={SpotifyIcon} link="https://middhunraghu.github.io/Spotify-Clone/" h3="Spotify" p="Audio Streaming WebApp"/>
             <ProjectCard src={freshBurger} link="" h3="Fresh Burger" p="Ham Burger Restaurant"/>
             <ProjectCard src={hipsster} link="" h3="CoolEye" p="Glasses Shop"/>
             <ProjectCard src={fitlift} link="" h3="FitLift" p="Fitness App"/>
