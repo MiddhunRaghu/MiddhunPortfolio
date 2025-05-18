bc5d99c4a8c09f2219d4e14177f07a3d6bdf3eb1
MiddhunRaghu <middhunraghu@gmail.com>
2025-05-18 18:59:43 +0530

    Resume updating

--- src/Sections/Projects/Projects.jsx
+++ src/Sections/Projects/Projects.jsx
@@ -2,7 +2,7 @@ import styles from './ProjectsStyles.module.css';
 import SpotifyIcon from '../../assets/Download Spotify logo on transparent background.jpg';
 import ProjectCard from '../../common/ProjectCard';
 import freshBurger from '../../assets/fresh-burger.png';
-import hipsster from '../../assets/hipsster.png';
+import WeatherIcon from '../../assets/weather-app.png';
 import fitlift from '../../assets/fitlift.png';
 
 function Projects() {
@@ -12,7 +12,7 @@ function Projects() {
         <div className={styles.projectsContainer}>
             <ProjectCard src={SpotifyIcon} link="https://middhunraghu.github.io/Spotify-Clone/" h3="Spotify" p="Audio Streaming WebApp"/>
             <ProjectCard src={freshBurger} link="" h3="Fresh Burger" p="Ham Burger Restaurant"/>
-            <ProjectCard src={hipsster} link="" h3="CoolEye" p="Glasses Shop"/>
+            <ProjectCard src={WeatherIcon} link="https://middhunraghu.github.io/Weather-Finder/" h3="Weather Finder" p="Weather Finder Application"/>
             <ProjectCard src={fitlift} link="" h3="FitLift" p="Fitness App"/>
         </div>
     </section>
