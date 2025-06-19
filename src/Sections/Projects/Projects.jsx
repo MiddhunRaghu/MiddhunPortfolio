157c8ae4b33567a61eac16221c520ee46f859d47
MiddhunRaghu <middhunraghu@gmail.com>
2025-06-19 12:26:32 +0530

    Add new logo in assets  and update prjects.jsx path

--- src/Sections/Projects/Projects.jsx
+++ src/Sections/Projects/Projects.jsx
@@ -1,7 +1,7 @@
 import styles from './ProjectsStyles.module.css';
 import SpotifyIcon from '../../assets/Download Spotify logo on transparent background.jpg';
 import ProjectCard from '../../common/ProjectCard';
-import freshBurger from '../../assets/fresh-burger.png';
+import ThirstyLion from '../../assets/roundlogo.png';
 import WeatherIcon from '../../assets/weather-app.png';
 import fitlift from '../../assets/fitlift.png';
 
@@ -11,7 +11,7 @@ function Projects() {
         <h1 className='sectionTitle'>Projects</h1>
         <div className={styles.projectsContainer}>
             <ProjectCard src={SpotifyIcon} link="https://middhunraghu.github.io/Spotify-Clone/" h3="Spotify" p="Audio Streaming WebApp"/>
-            <ProjectCard src={freshBurger} link="" h3="Fresh Burger" p="Ham Burger Restaurant"/>
+            <ProjectCard src={ThirstyLion} link="https://MiddhunRaghu.github.io/Thirsty-Lion-Coffee-Shop/" h3="Thirsty Lion" p="Thirsty Lion - Coffee Shop"/>
             <ProjectCard src={WeatherIcon} link="https://middhunraghu.github.io/Weather-Finder/" h3="Weather Finder" p="Weather Finder Application"/>
             <ProjectCard src={fitlift} link="" h3="FitLift" p="Fitness App"/>
         </div>
